8a247968ead45d2abe10a2caed5f483094c2e827
huelva91 <huelva91@users.noreply.github.com>
2022-11-23 23:40:00 +0100

    Ejercicio Ficheros

---
+++ Ejercicios/Ejercicio11.Ficheros/Clases.py
@@ -0,0 +1,52 @@
+class Colegio:
+    def __init__(self, nombre_colegio, nombre_alumno, apellidos_alumno, dni_alumnos, asignaturas=[]):
+        self._nombre_colegio = nombre_colegio
+        self._nombre_alumno = nombre_alumno
+        self._apellidos_alumno = apellidos_alumno
+        self._dni_alumnos = dni_alumnos
+        self._asignaturas = asignaturas
+
+        # Atributo  nombre colegio
+        @property
+        def nombre_colegio(self):
+            return self._nombre_colegio
+
+        @nombre_colegio.setter
+        def nombre(self, nombre_colegio):
+            self._nombre_colegio = nombre_colegio
+
+        # Atributo  nombre_alumno
+        @property
+        def nombre_alumno(self):
+            return self._nombre_alumno
+
+        @nombre_alumno.setter
+        def nombre_alumno(self, nombre_alumno):
+            self._nombre_alumno = nombre_alumno
+
+        # Atributo  apellidos_alumno
+        @property
+        def apellidos_alumno(self):
+            return self._apellidos_alumno
+
+        @apellidos_alumno.setter
+        def apellidos_alumno(self, apellidos_alumno):
+            self._apellidos_alumno = apellidos_alumno
+
+        # Atributo  dni_alumnos
+        @property
+        def dni_alumnos(self):
+            return self._nombre_alumno
+
+        @dni_alumnos.setter
+        def dni_alumnos(self, dni_alumnos):
+            self._dni_alumnos = dni_alumnos
+
+        # Atributo  asignaturas
+        @property
+        def asignaturas(self):
+            return self._asignaturas
+
+        @asignaturas.setter
+        def asignaturas(self, asignaturas):
+            self._asignaturas = asignaturas
---
+++ Ejercicios/Ejercicio11.Ficheros/Lectura.py
@@ -0,0 +1,25 @@
+from Clases import *
+import logging as log
+
+log.basicConfig(level=log.INFO,
+                format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('logs/Bar_Cafe.log'),
+                    log.StreamHandler()
+                ])
+lista_colegios = []
+
+with open('alumnos-colegio.txt','r', encoding='utf8') as archivo:
+    for linea in archivo:
+        lista_datos = linea.rstrip().split("|")
+        colegio = Colegio(lista_datos[0], lista_datos[1], lista_datos[2], lista_datos[3], lista_datos[4])
+
+
+
+
+# print(nombre_colegio)
+# nombre_alumno =
+
+
+
