6fcee25e42af26081c0f1399c06fc38647a58b4c
huelva91 <huelva91@users.noreply.github.com>
2022-11-24 10:31:15 +0100

    Corregir clases

--- Ejercicios/Ejercicio11.Ficheros/Lectura.py
+++ Ejercicios/Ejercicio11.Ficheros/Lectura.py
@@ -5,21 +5,15 @@ log.basicConfig(level=log.INFO,
                 format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
                 datefmt='%I:%M:%S %p',
                 handlers=[
-                    log.FileHandler('logs/Bar_Cafe.log'),
+                    log.FileHandler('logs/alumnos.log'),
                     log.StreamHandler()
                 ])
 lista_colegios = []
-
-with open('alumnos-colegio.txt','r', encoding='utf8') as archivo:
+with open('alumnos-colegio.txt', 'r', encoding='utf8') as archivo:
     for linea in archivo:
+        log.debug(linea)
         lista_datos = linea.rstrip().split("|")
-        colegio = Colegio(lista_datos[0], lista_datos[1], lista_datos[2], lista_datos[3], lista_datos[4])
-
-
-
+        colegio = Colegio(lista_datos[1], lista_datos[2], lista_datos[3], lista_datos[4])
 
 # print(nombre_colegio)
 # nombre_alumno =
-
-
-
