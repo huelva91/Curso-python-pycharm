6b0694286149a06d800ae6051a0ee910031b7635
huelva91 <huelva91@users.noreply.github.com>
2022-11-24 11:01:43 +0100

    Base de datos

---
+++ creacionBBDD.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `curso_python`.`productos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL,
+  `precio` INT NULL,
+  `fecha_registro` DATETIME NULL,
+  PRIMARY KEY (`id`));
